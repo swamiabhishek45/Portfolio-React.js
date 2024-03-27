9cee794c44aa048a70fd9e3cb538e0204e47834e
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-03-27 13:44:55 +0530

    add github calender

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useLoaderData, Link } from "react-router-dom";
+import GitHubCalendar from "react-github-calendar";
 
 function Github() {
 
@@ -16,13 +17,17 @@ function Github() {
   // }, []);
   return (
     <>
-    <div className="bg-gray-800 text-white p-12 text-3xl text-center flex flex-col justify-center items-center gap-4">
-      Github Followers: {data.followers}
-      
-      <img src={data.avatar_url} alt="profile pic" width={300} className="" />
-      
-
-    </div>
+      <div className="bg-gray-800 text-white p-12 text-3xl text-center flex flex-col justify-center items-center gap-4">
+        Github Followers: {data.followers}
+        <img src={data.avatar_url} alt="profile pic" width={300} className="" />
+        <GitHubCalendar
+          username="swamiabhishek45"
+          blockSize={15}
+          blockMargin={5}
+          color="#39d353"
+          fontSize={16}
+        />
+      </div>
     </>
   );
 }
