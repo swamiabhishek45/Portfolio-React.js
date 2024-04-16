be47a0aff5e858a978c9a0df2b9147ad9ef38f85
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-04-16 21:50:37 +0530

    project section completed

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -32,7 +32,7 @@ function Projects() {
                     />
                     <Card
                         src={MernChatApp}
-                        name="Demo Project"
+                        name="Chat Bee - Chat Application"
                         desc="Chat Bee is chat application build with the power of MERN Stack. This application allows user to chat in real-time communication."
                         T1="NodeJs"
                         T2="React"
@@ -43,8 +43,8 @@ function Projects() {
                     />
                     <Card
                         src={Blog}
-                        name="Demo Project"
-                        desc="This is simple demo project"
+                        name="Tree Trop Tales - Blog App"
+                        desc="Developed a Blog Application in which users can post their though, ideas by writing blogs with images. It has authentication functionlity."
                         T1="React"
                         T2="Tailwind CSS"
                         T3="Appwrite"
@@ -54,55 +54,55 @@ function Projects() {
                     />
                     <Card
                         src={Soccer}
-                        name="Demo Project"
-                        desc="This is simple demo project"
+                        name="Animating Website"
+                        desc="I've crafted a dynamic and engaging soccer club website. With smooth animations and transitions powered by GSAP."
                         T1="JavaScript"
-                        T2="#CSS"
-                        T3="#JavaScript"
+                        T2="CSS"
+                        T3="GSAP"
                         btn="Get Code"
                         LiveLink="https://soccer-club-eta.vercel.app/"
                         GithubLink="https://github.com/swamiabhishek45/Animating-Website"
                     />
                     <Card
                         src={Bubble}
-                        name="Demo Project"
-                        desc="This is simple demo project"
-                        T1="#HTML"
-                        T2="#CSS"
-                        T3="#JavaScript"
+                        name="Bubble Game"
+                        desc="Dive into a world of delightful distraction with our charming bubble game site! Crafted with love using HTML, CSS, and JavaScript"
+                        T1="HTML"
+                        T2="CSS"
+                        T3="JavaScript"
                         btn="Get Code"
                         LiveLink="https://bubble-bash-ten.vercel.app/"
                         GithubLink="https://github.com/swamiabhishek45/Bubble-Game"
                     />
                     <Card
                         src={ChatApp}
-                        name="Demo Project"
-                        desc="This is simple demo project"
-                        T1="#HTML"
-                        T2="#CSS"
-                        T3="#JavaScript"
+                        name="Mystery Meet - Chat App"
+                        desc="Implemented a own chat app using socket.io library. As the name mystrey you can chat with any random person this is the uniqueness of this app."
+                        T1="HTML"
+                        T2="JavaScript"
+                        T3="Socket.IO"
                         btn="Get Code"
                         LiveLink=""
                         GithubLink="https://github.com/swamiabhishek45/MysteryMeet-Chat-App"
                     />
                     <Card
                         src={Todo}
-                        name="Demo Project"
-                        desc="This is simple demo project"
-                        T1="#HTML"
-                        T2="#CSS"
-                        T3="#JavaScript"
+                        name="Add Todo"
+                        desc="Created a simple yet efficient to-do website. Storing to-dos locally using localStorage enhances accessibility and persistence."
+                        T1="ReactJs"
+                        T2="TailwindCSS"
+                        T3="Context API"
                         btn="Get Code"
                         LiveLink=""
                         GithubLink="https://github.com/swamiabhishek45/React.js-Learning/tree/main/Projects/10todoContextLocal"
                     />
                     <Card
                         src={Spotify}
-                        name="Demo Project"
-                        desc="This is simple demo project"
-                        T1="#HTML"
-                        T2="#CSS"
-                        T3="#JavaScript"
+                        name="Spotify Clone"
+                        desc="This app provides you a smooth experience with responsiveness. It has songs that you like. This app is responsive in small devices also."
+                        T1="HTML"
+                        T2="CSS"
+                        T3="JavaScript"
                         btn="Get Code"
                         LiveLink=""
                         GithubLink="https://github.com/swamiabhishek45/Spotify-Clone"
